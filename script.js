8245044b6cad9e036955cf53cb77c3045d48935c
Carioca01 <88631657+Carioca01@users.noreply.github.com>
2023-11-14 13:45:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var pergunta = "Qual é o seu Primeiro nome obs 1 letra maiuscula resto minuscula";
+var nome = prompt (pergunta);
+var Flavia = "Flavia"
+var Flavia1 = "flavia"
+if(nome == Flavia) {
+    var saudação = "Só penso em voçe, " +nome+ " o dia todo ate no momento do estudo";
+    alert (saudação);   
+    break;
+}
+
+
+else {
+    var saudação = "VOCÊ NÃO TEM A CHAVE DO MEU CORACÃO !";
+    alert (saudação)}
